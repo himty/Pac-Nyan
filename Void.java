98f03dbafe56031b01e169d14bec1045ce68ffc6
himty <jenny.wang244@gmail.com>
2018-05-03 12:10:54 -0700

    Initial commit
    placeholder in the map

---
+++ Void.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Void here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Void extends GridActor
+{
+    private static final int DEFAULT_SIZE = 30;
+    
+    public Void() {
+        getImage().scale(DEFAULT_SIZE, DEFAULT_SIZE);
+    }
+    
+    /**
+     * Act - do whatever the Void wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
