5e06321a04ee0ed8b3bd0456ade2bdf289fd2dff
himty <jenny.wang244@gmail.com>
2018-05-10 12:19:05 -0700

    Add movement in non-Gridworld environment

--- PacNyan.java
+++ PacNyan.java
@@ -7,14 +7,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 import greenfoot.Color;
-public class PacNyan extends GridActor
+public class PacNyan extends Actor
 {
+    private boolean isMovingLeft = true;
+    int counter = 50;
+    
     public PacNyan()
     {
-        GreenfootImage image = getImage();
-        image.scale(image.getWidth() - 213 , image.getHeight() - 275);//diff = 60
-        setImage(image);
-        
+        getImage().scale(48, 36);
     }
     /**
      * Act - do whatever the PacNyan wants to do. This method is called whenever
@@ -22,6 +22,55 @@ public class PacNyan extends GridActor
      */
     public void act() 
     {
-        // Add your action code here.
+        if (counter > 0) {
+            moveLeft();
+            
+        }
+        else if (counter > -50){
+            moveRight();
+        }
+        else if (counter > -100) {
+            moveUp();
+        }
+        else if (counter > -150) {
+            moveDown();
+        }
+        counter--;
     }    
+    
+    public void moveLeft() {
+        setLocation(getX() - 1, getY());
+        
+        //flip the image
+        if(!isMovingLeft) {
+            setImage(getReverseImage(getImage()));
+            isMovingLeft = true;
+        }
+    }
+    
+    public void moveRight() {
+        setLocation(getX() + 1, getY());
+        
+        //flip the image
+        if(isMovingLeft) {
+            setImage(getReverseImage(getImage()));
+            isMovingLeft = false;
+        }
+    }
+    
+    public void moveUp() {
+        setLocation(getX(), getY() - 1);
+    }
+    
+    public void moveDown() {
+        setLocation(getX(), getY() + 1);
+    }
+    
+    public GreenfootImage getReverseImage(GreenfootImage img) {
+        int w = img.getWidth(), h = img.getHeight();
+        GreenfootImage image = new GreenfootImage(w, h);
+        for (int y=0; y<h; y++) for (int x=1; x<=w; x++)
+            image.setColorAt(w-x, y, img.getColorAt(x-1, y));
+        return image;
+    }
 }
