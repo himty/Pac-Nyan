cb0564fc0e8e73e0353bad47fce98a1f66afc6ed
himty <jenny.wang244@gmail.com>
2018-05-17 11:50:53 -0700

    Add random movement

--- Clyde.java
+++ Clyde.java
@@ -8,84 +8,51 @@ import java.util.ArrayList;
  * @version (a version number or a date)
  */
 public class Clyde extends Ghost
-{    
-    /**
-     * Number of steps to left to take before making a decision to turn
-     */
-    private int numMoreSteps;
-    
-    /**
-     * The maximum number of steps Clyde can take before making a decision
-     * to turn or not to turn
-     */
-    private final static int INDECICITIVITY = 2;
-    
+{        
+   private static final int CELL_SIZE = 30;
+   private int counter;
+   private int currDirection;
+   private String currDirectionStr;
+   private int prevDirection;
+
     public Clyde() {
-        getImage().scale(30, 30);
-        numMoreSteps = (int)(Math.random() * INDECICITIVITY);
+        getImage().scale(CELL_SIZE, CELL_SIZE);
+        currDirectionStr = "left";
+        currDirection = 1;
     }
+    
     /**
      * Act - do whatever the Clyde wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        /*
-            //turn left, turn right, or keep going straight?
-            double rand = Math.random();
-            if(rand < 0.2) {
-               if(!hasWallOnLeft()) {
-                    turnLeft();
+        if (counter == 0 || !canMove(currDirectionStr)) {
+            System.out.println("x: " + getX() + " y: " + getY());
+            counter = CELL_SIZE * 4;
+            prevDirection = currDirection;
+            do {
+                System.out.println(currDirectionStr + "?");
+                currDirection = (int)(Math.random() * 10);
+                if (currDirection == 0) {
+                    currDirectionStr = "up";
                 }
-                else if(!hasWallOnRight()) {
-                    turnRight();
+                else if (currDirection == 1) {
+                    currDirectionStr = "left";
                 }
-            }
-            else if (rand > 0.8 || hasWallInFront()) {
-                if(!hasWallOnRight()) {
-                    turnRight();
+                else if (currDirection == 2) {
+                    currDirectionStr = "down";
                 }
-                else if (!hasWallOnLeft()){
-                    turnLeft();
+                else if (currDirection == 3) {
+                    currDirectionStr = "right";
                 }
-            }                 
-        move(1);
-        */
+            } while (!canMove(currDirectionStr));
+            System.out.println("picked " + currDirectionStr);
+        }
+        
+        if (canMove(currDirectionStr)) {
+            move(currDirectionStr);
+        }
+        counter--;
     } 
-    
-    public void turnLeft() {
-        setRotation(getRotation() + 270);
-        getImage().rotate(90);
-    }
-    
-    public void turnRight() {
-        setRotation(getRotation() + 90);
-        getImage().rotate(270);
-    }
-    
-    public void turnAround() {
-        setRotation(getRotation() + 180);
-        getImage().rotate(180);
-    }
-    
-    public boolean hasWallInFront() {
-           return (getRotation() == 0 && getOneObjectAtOffset(1, 0, Wall.class) != null)
-                || (getRotation() == 90 && getOneObjectAtOffset(0, 1, Wall.class) != null)
-                || (getRotation() == 180 && getOneObjectAtOffset(-1, 0, Wall.class) != null)
-                || (getRotation() == 270 && getOneObjectAtOffset(0, -1, Wall.class) != null);
-    }
-    
-    public boolean hasWallOnLeft() {
-           return (getRotation() == 0 && getOneObjectAtOffset(0, -1, Wall.class) != null)
-                || (getRotation() == 90 && getOneObjectAtOffset(1, 0, Wall.class) != null)
-                || (getRotation() == 180 && getOneObjectAtOffset(0, 1, Wall.class) != null)
-                || (getRotation() == 270 && getOneObjectAtOffset(-1, 0, Wall.class) != null);
-    }
-    
-    public boolean hasWallOnRight() {
-           return (getRotation() == 0 && getOneObjectAtOffset(0, 1, Wall.class) != null)
-                || (getRotation() == 90 && getOneObjectAtOffset(-1, 0, Wall.class) != null)
-                || (getRotation() == 180 && getOneObjectAtOffset(0, -1, Wall.class) != null)
-                || (getRotation() == 270 && getOneObjectAtOffset(1, 0, Wall.class) != null);
-    }
 }
