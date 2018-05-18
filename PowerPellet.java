9c50c58add20e16386d510435291757f541ad228
character11 <38892310+character11@users.noreply.github.com>
2018-05-18 12:00:23 -0700

    Update PowerPellet.java

--- PowerPellet.java
+++ PowerPellet.java
@@ -6,20 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PowerPellet extends GridActor
+public class PowerPellet extends Food
 {
-    public PowerPellet()
-    {
-        GreenfootImage image = getImage();
-        image.scale(image.getWidth() - (120*8) , image.getHeight() - (120*6));//diff = 60
-        setImage(image);
-    }
     /**
      * Act - do whatever the PowerPellet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        getImage().scale( 130,100);
     }    
 }
