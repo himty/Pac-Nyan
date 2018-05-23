4c0553a0fd46304749ffc5f29234cc4a0d091248
himty <jenny.wang244@gmail.com>
2018-05-23 14:38:09 -0700

    Added AI

--- Pinky.java
+++ Pinky.java
@@ -1,22 +1,73 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
- * Write a description of class Pinky here.
+ * Pinky is a ghost that tries to ambush the PacNyan
  * 
- * @author (your name) 
+ * @author Jenny Wang
  * @version (a version number or a date)
  */
 public class Pinky extends Ghost
 {
+    private String currDirection;
+    private int counter;
+    
     public Pinky() {
         getImage().scale(30, 30);
+        currDirection = "-";
     }
+    
     /**
      * Act - do whatever the Pinky wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        List<PacNyan> list = getWorld().getObjects(PacNyan.class);
+        if (list.size() > 0) {
+            PacNyan pac = list.get(0);
+            int pacX = pac.getXCoord();
+            int pacY = pac.getYCoord();
+            String pacDir = pac.getDirection();
+
+            if (getX() <= pacX) {
+                System.out.println("on left ");
+                //Pinky is on the left. Close in on the pacNyan
+                if (getY() < pacY && canMove("down")) {
+                    currDirection = "down";
+                }
+                else if (getY() > pacY && canMove("up")) {
+                    currDirection = "up";
+                }
+                else if (canMove("right")){
+                    currDirection = "right";
+                }
+                else {
+                    //if this is a dead end
+                    currDirection = "left";
+                }
+            }
+            else {
+                //Pinky is on the right. keep going left
+                if (canMove("left")) {
+                    currDirection = "left";
+                }
+                else if (getY() < pacY && canMove("down")) {
+                    currDirection = "down";
+                }
+                else if (getY() > pacY && canMove("up")) {
+                    currDirection = "up";
+                }
+                else {
+                    //if this is a dead end
+                    currDirection = "right";
+                }
+            }
+
+            counter--;
+            if (canMove(currDirection)) {
+                move(currDirection);
+            }
+        }
     }    
 }
