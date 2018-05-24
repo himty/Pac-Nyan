3dc3d32c70237d9c9d2efea8e8732d595db3dd64
character11 <38892310+character11@users.noreply.github.com>
2018-05-23 22:50:23 -0700

    Update PacNyan.java
    - generate fruit (needs work)
    - eat dots/pellets + update points (is not updated on menu)
    - ghostTimer added for generation

--- PacNyan.java
+++ PacNyan.java
@@ -24,6 +24,8 @@ public class PacNyan extends MazeActor
     private static final int WIDTH = 40;
     private static final int HEIGHT = 46;
     private static final int CELL_SIZE = 30;
+    private int points;
+    private int ghostTimer;
     
     public PacNyan()
     {
@@ -33,6 +35,8 @@ public class PacNyan extends MazeActor
         lastKey = "-";
         queuedKey = lastKey;
         currDirection = lastKey;
+        points = 0;
+        ghostTimer = 0;
     }
     
     /**
@@ -80,6 +84,51 @@ public class PacNyan extends MazeActor
                  queuedKey = lastKey;
             }
         }
+        if (getWorld().getObjects(Fruit.class).isEmpty()){generateFruit();}
+        eat();
+    }
+    public void generateFruit()
+    {
+        Actor fruit = new Fruit();
+        int[] genPtRow = new int[]
+        {5, 9, 15, 19, 23}; 
+        int[] genPtCol = new int[]
+        {1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+        int randoX = genPtRow[(int)(Math.random() * (genPtRow.length - 1))];
+        int randoY = genPtCol[(int)(Math.random() * (genPtCol.length - 1))];
+        if(getWorld().getObjects(Fruit.class).isEmpty() && Greenfoot.getRandomNumber(10000) > 9997) 
+        {
+            getWorld().addObject(fruit, randoX, randoY);
+            fruit.setLocation(getX(), getY() - 10);
+        }
+    }
+    public void eat()
+    {
+        Actor dot = getOneObjectAtOffset(0,0, PacDot.class); 
+        Actor powa = getOneObjectAtOffset(0,0, PowerPellet.class); 
+        Actor fruit = getOneObjectAtOffset(0,0, Fruit.class);
+        
+        if (dot != null)
+        {
+            getWorld().removeObject(dot);
+            points += 10;
+        }
+        else if (powa != null)
+        {
+            getWorld().removeObject(powa);
+            // turnOnScareMode();
+            // ghostTimer = 0;
+            points += 50;
+        }
+        else if (fruit != null)
+        {
+            getWorld().removeObject(fruit);
+            points += 100;
+        }
+    }
+    public int getTotalPoints()
+    {
+        return points;
     }
     
     public void setImageDir(String dir) {
