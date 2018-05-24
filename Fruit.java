bf32f0c863ff37dbf48994f1a2144860a4696f7d
character11 <38892310+character11@users.noreply.github.com>
2018-05-24 01:53:58 -0700

    Create Fruit.java
    keeps a counter (in computer ticks) as timer

---
+++ Fruit.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fruit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fruit extends Food
+{
+    private int time;
+    public Fruit()
+    {
+        time = 0;
+    }
+    /**
+     * Act - do whatever the Fruit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        getImage().scale( 30,30);
+        if ( time == 500 ) 
+        {
+            getWorld().removeObject(this);
+        }
+        else
+        {
+            time++;
+        }
+    }    
+}
